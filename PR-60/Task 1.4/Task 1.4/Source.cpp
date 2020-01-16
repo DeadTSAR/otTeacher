8fc0b606a59e692cf521c113aafe4e099b62dced
Vladimir <41343658+DeadTSAR@users.noreply.github.com>
2020-01-16 20:38:05 +0300

    fedUP

--- PR-60/Task 1.4/Task 1.4/Source.cpp
+++ PR-60/Task 1.4/Task 1.4/Source.cpp
@@ -1,5 +1,5 @@
 ﻿/*
-	Составить программу, которая в каждой строке матрицы G(m,n), m≤10, n≤12 находит сумму элементов, 
+	Составить программу, которая в каждой строке матрицы G(m,n), m?10, n?12 находит сумму элементов, 
 	расположенных до максимального элемента и после максимального элемента. Если сумма не может быть
 	вычислена (нет элементов до или после максимального элемента), то считать ее равной нулю. Вывести 
 	исходную матрицу, располагая в одной строке элементы строки матрицы, после которых вывести номер 
@@ -8,3 +8,104 @@
 	которые должны вызываться в функции main(). Выделение памяти под матрицу производится динамически
 */
 
+#include <iostream>
+#include <time.h>
+using namespace std;
+
+int** creat_mas(int N, int M) {
+    int** mas;
+    mas = new int* [N];
+    for  ( int i = 0;  i < N;  i++)
+    {
+        mas[i] = new int[M];
+    }
+    return mas;
+}
+
+void mas_init(int** mas, int N, int M) {
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < M; j++)
+        {
+            mas[i][j] = rand() % 20 - 10;
+        }
+    }
+}
+
+int* max_elem_in_str(int** mas, int str, int M) {
+    int* mas_max = new int[2];
+    int max = INT_MIN;
+    for (int i = 0; i < M; i++){
+        if (mas[str][i] > max) {
+            mas_max[0] = i + 1;
+            max = mas[str][i];
+            mas_max[1] = max;
+        }
+    }
+    return mas_max;
+}
+
+void sum(int** mas, int str, int M) {
+    int sumMas[2] = {0, 0};
+    int current_sum = 0;
+    int index = 0;
+    int* mas_max = max_elem_in_str(mas, str, M);
+
+    for (int i = 0; i < M; i++) {
+        if(mas_max[1] == mas[str][i]){
+            index = 1;
+            sumMas[index] = current_sum;
+            current_sum = 0;
+            continue;
+        }
+        current_sum += mas[str][i];
+        sumMas[index] = current_sum;
+    }
+
+    cout << sumMas[0] << "\t\t" << sumMas[1];
+
+}
+
+void mas_print_or(int** mas, int N, int M) {
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < M; j++)
+        {
+            cout << mas[i][j] << "\t";
+        }
+        cout << endl;
+    }
+}
+
+
+void mas_print(int** mas, int N, int M) {
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < M; j++)
+        {
+            cout << mas[i][j] << "\t";
+        }
+        int* mas_max = max_elem_in_str(mas, i, M);
+        cout <<"index " << mas_max[0] << "\t\t" << "max " << mas_max[1] << "\t";
+
+        sum(mas, i, M);
+        cout << endl;
+    }
+}
+
+int main() {
+    srand(time(NULL));
+    const int N = 10;
+    const int M = 10;
+
+    // Создаем и инициализируем исходный массив mas;
+    int** mas = creat_mas(N, M);
+    mas_init(mas, N, M);
+
+    mas_print_or(mas, N, M);
+    cout << endl;
+    cout << endl;
+
+    mas_print(mas, N, M);
+    cout << endl;
+}
---
+++ PR-60/task1.2/main (1).cpp
@@ -0,0 +1,126 @@
+﻿/*
+ * 2.	Составить программу, которая в матрице A(m,n), m?10, n?12, меняет местами строку, содержащую максимальный
+ * элемент со строкой, содержащей минимальный элемент. Предполагается, что искомые элементы единственные.
+ * Вывести исходную и преобразованную матрицы, минимальный и максимальный элементы, а также номера строк, в которых
+ * они расположены. Если минимальный и максимальный элементы расположены в одной строке, то поменять местами столбцы,
+ * содержащие эти элементы. Следует реализовать ввод исходного массива, вывод требуемого массива и подпрограмму
+ * реализации работы с массивом в отдельных функциях, которые должны вызываться в функции main(). Выделение памяти под
+ * матрицу производится динамически
+ * */
+
+
+#include <iostream>
+#include <time.h>
+#include <random>
+
+using namespace std;
+
+int ** mas_creation(int N, int M){
+    int ** mas;
+    mas = new int * [N];
+    for (int i = 0; i < N; i++) {
+        mas[i] = new int[M];
+    }
+    return mas;
+}
+
+void mas_init(int ** mas, int N, int M){
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < M; j++) {
+            mas[i][j] = 1 + rand() % 1000 ;
+        }
+    }
+}
+
+void find_min(int ** mas, int N, int M, int & min_element, int & min_str, int & min_colum){
+    min_element = mas[0][0];
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < M; j++) {
+            if(min_element > mas[i][j]){
+                min_element = mas[i][j];
+                min_str = i;
+                min_colum = j;
+            }
+        }
+    }
+}
+
+void find_max(int ** mas, int N, int M, int & max_element, int & max_str, int & max_colum){
+    max_element = mas[0][0];
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < M; j++) {
+            if(max_element < mas[i][j]){
+                max_element = mas[i][j];
+                max_str = i;
+                max_colum = j;
+            }
+        }
+    }
+}
+
+void change_mas(int ** mas, int & min_str, int & max_str, int & min_colum, int & max_colum, int N){
+    if(min_str == max_str){
+        for (int i = 0; i < N; i++) {
+            int current = mas[i][max_colum];
+            mas[i][max_colum] = mas[i][min_colum];
+            mas[i][min_colum] = current;
+        }
+    }
+    else {
+        int *min = mas[min_str];
+        mas[min_str] = mas[max_str];
+        mas[max_str] = min;
+    }
+}
+
+void mas_print(int ** mas, int N, int M){
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < M; j++) {
+            cout << mas[i][j] << "\t";
+        }
+        cout << endl;
+
+    }
+
+}
+
+int main() {
+
+    srand(time(NULL));
+
+    const int N = 10;
+    const int M = 12;
+
+    int min_element;
+    int min_str;
+    int min_colum;
+    int max_element;
+    int max_str;
+    int max_colum;
+
+
+    int ** mas = mas_creation(N, M);
+    mas_print(mas, N, M);
+    cout << endl;
+
+    mas_init(mas, N, M);
+    mas_print(mas, N, M);
+    cout << endl;
+
+    find_min(mas, N, M, min_element, min_str, min_colum);
+    cout <<"min_element " << min_element << " min_str " << min_str;
+    cout << endl;
+
+    find_max(mas, N, M, max_element, max_str, max_colum);
+    cout <<"max_element " << max_element << " max_str " << max_str;
+    cout << endl;
+
+    cout << endl;
+
+    change_mas(mas, min_str, max_str, min_colum, max_colum, N);
+    mas_print(mas, N, M);
+    cout << endl;
+
+
+    return 0;
+}
